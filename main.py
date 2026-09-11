8235bc2503859ac44b76839e6c6cc96eae165a95
Bapo <122837657+bapo2@users.noreply.github.com>
2026-09-11 18:18:45 -0400

    chore: Initialize project

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+"""
+Placeholder main.py entry point. Nothing implemented yet.
+"""
+
+def main():
+    raise NotImplementedError("Nothing to see yet!")
+
+if __name__ == "__main__":
+    main()
